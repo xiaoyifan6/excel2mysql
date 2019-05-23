0bf50139c52b33151cc4b8dfd9631d9d7cb7376f
yangzhi.ou <1570395652@qq.com>
2019-05-23 08:51:12 +0800

    更新依赖包版本

--- test/excel-mysql.test.js
+++ test/excel-mysql.test.js
@@ -1,40 +1,41 @@
-var should = require('should');
-var excel2mysql = require('../');
+var should = require("should");
+var excel2mysql = require("../");
 
-describe('xlsx to mysql', function () {
-    // it('should convert xlsx to sql ', function () {
-    //     excel2mysql({
-    //         input: './sample/sample.xls',
-    //     }, function (err, sql, result) {
-    //         should.not.exist(err)
-    //         // result.should.be.an.instanceOf(Object)
-    //         err && console.error(err);
+describe("xlsx to mysql", function() {
+  // it('should convert xlsx to sql ', function () {
+  //     excel2mysql({
+  //         input: './sample/sample.xls',
+  //     }, function (err, sql, result) {
+  //         should.not.exist(err)
+  //         // result.should.be.an.instanceOf(Object)
+  //         err && console.error(err);
 
-    //         sql && console.log(sql);
-    //     })
-    // })
+  //         sql && console.log(sql);
+  //     })
+  // })
 
-    it('should convert xlsx to mysql', function () {
-        excel2mysql({
-            input: './sample/sample.xls',
-            model: 'delete',
-            mysql: {
-                host: '127.0.0.1',
-                user: 'work',
-                password: 'work',
-                port: '3306',
-                database: 'test'
-            }
-        }, function (err, sql, result) {
-            should.not.exist(err)
-            // result.should.be.an.instanceOf(Object)
-            err && console.error(err);
+  it("should convert xlsx to mysql", function() {
+    excel2mysql(
+      {
+        input: "./sample/sample.xls",
+        model: "diff",
+        mysql: {
+          host: "127.0.0.1",
+          user: "work",
+          password: "work",
+          port: "3306",
+          database: "test"
+        }
+      },
+      function(err, sql, result) {
+        should.not.exist(err);
+        // result.should.be.an.instanceOf(Object)
+        err && console.error(err);
 
-            sql && console.log(sql.sql);
+        sql && console.log(sql.sql);
 
-            result && console.log(result);
-        })
-    })
-
-
+        result && console.log(result);
+      }
+    );
+  });
 });
